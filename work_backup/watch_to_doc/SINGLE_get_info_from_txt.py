9e70ddb49de86d0921bc5fb5ad7b5f2fe69208dc
asdbnm999 <alex.porubaev@mail.ru>
2024-08-19 21:16:24 +0300

    Update SINGLE_get_info_from_txt.py

--- work_backup/watch_to_doc/SINGLE_get_info_from_txt.py
+++ work_backup/watch_to_doc/SINGLE_get_info_from_txt.py
@@ -1,95 +1,105 @@
+import pprint
 from deep_translator import GoogleTranslator
+import PyPDF2
 
-itdbKey = str()
-webInfKey = str()                       # keys_checked_flag
-incidentGroup = str()
-incidentDate = str()
 
-incidentCountry_ru = str()
-incidentCountry_en = str()
+def get_txt(pdf):
+    with open(pdf, 'rb') as file:
+        reader = PyPDF2.PdfReader(file)
+        text = ""
 
-incidentLocation_ru = str()
-incidentLocation_en = str()
+        for page in reader.pages:
+            text += page.extract_text() + "\n"
 
-incidentDetectSummary = str()
-incidentType = str()
-incidentDangerCategories = list()       # idc_flag
-incidentText = str()
+# Печать извлеченного текста
+    text = text.split('\n')
+    return text
 
-keys_checked_flag = False
-idc_flag = False
 
-with open('extracted_text.txt', 'r', encoding='utf-8') as text_file:
-    source_text = text_file.readlines()
+def get_docx_info(pdf):
+    itdbKey = str()
+    webInfKey = str()                       # keys_checked_flag
+    incidentGroup = str()
+    incidentDate = str()
 
-print(source_text)
+    incidentCountry_ru = str()
+    incidentCountry_en = str()
 
-for i in range(len(source_text)):
-    try:
-        if 'WebINF Key' in source_text[i]:
-            if '2024' in source_text[i] and not keys_checked_flag:
-                itdbKey = source_text[i].split('WebINF Key: ')[1].strip()
-                webInfKey = f'({source_text[i+1].strip()})'
-                keys_checked_flag = True
-            elif '2024' not in source_text[i] and not keys_checked_flag:
-                # print('\n\n\n\n\n\n\n')
-                itdbKey = source_text[i+1].strip()
+    incidentLocation_ru = str()
+    incidentLocation_en = str()
+
+    incidentDetectSummary = str()
+    incidentType = str()
+    incidentDangerCategories = str()       # idc_flag
+    incidentText = str()
+
+    keys_checked_flag = False
+    idc_flag = False
+
+    # 2024-01-001.pdf
+    source_text = get_txt(pdf)
+    pprint.pprint(source_text)
+    for i in range(len(source_text)):
+        try:
+            if 'WebINF Key' in source_text[i]:
+                if '2024' in source_text[i] and not keys_checked_flag:
+                    itdbKey = source_text[i].split('WebINF Key: ')[1].strip()
+                    webInfKey = f'({source_text[i+1].strip()})'
+                    keys_checked_flag = True
+                elif '2024' not in source_text[i] and not keys_checked_flag:
+                    # print('\n\n\n\n\n\n\n')
+                    itdbKey = source_text[i+1].strip()
+                    webInfKey = f'({source_text[i+2].strip()})'
+                    keys_checked_flag = True
+
+            elif 'WebINF' in source_text[i] and 'Key:' in source_text[i+1]:
+                itdbKey = source_text[i+1].split('Key: ')[1].strip()
                 webInfKey = f'({source_text[i+2].strip()})'
                 keys_checked_flag = True
 
-        elif 'WebINF' in source_text[i] and 'Key:' in source_text[i+1]:
-            itdbKey = source_text[i+1].split('Key: ')[1].strip()
-            webInfKey = f'({source_text[i+2].strip()})'
-            keys_checked_flag = True
+            elif 'Incident Group' in source_text[i]:
+                incidentGroup = source_text[i].split('Incident Group: ')[1].split('Location Details:')[0][6:].strip()
+                webInfKey += f'\nИнцидент\n{incidentGroup} группы'
 
-        elif 'Incident Group' in source_text[i]:
-            incidentGroup = source_text[i].split('Incident Group: ')[1].split('Location Details:')[0][6:].strip()
-            webInfKey += f'\nИнцидент\n{incidentGroup} группы'
+            elif 'Incident Date' in source_text[i] and 'Country' in source_text[i]:
+                united_list = source_text[i].split('Incident Date: ')[1].split('Country:')
+                incidentDate = GoogleTranslator(source='en', target='ru').translate(united_list[0].strip())
+                if incidentDate[-2:] != 'г.':
+                    incidentDate += ' г.'
+                incidentCountry_en = united_list[1].strip()
+                incidentCountry_ru = GoogleTranslator(source='en', target='ru').translate(incidentCountry_en)
+                incidentCountry_en = '(' + incidentCountry_en + ')'
 
-        elif 'Incident Date' in source_text[i] and 'Country' in source_text[i]:
-            united_list = source_text[i].split('Incident Date: ')[1].split('Country:')
-            incidentDate = GoogleTranslator(source='en', target='ru').translate(united_list[0].strip())
-            if incidentDate[-2:] != 'г.':
-                incidentDate += ' г.'
-            incidentCountry_en = united_list[1].strip()
-            incidentCountry_ru = GoogleTranslator(source='en', target='ru').translate(incidentCountry_en)
-            incidentCountry_en = '(' + incidentCountry_en + ')'
+            elif 'Incident Type' in source_text[i]:
+                united_list = source_text[i].split('Incident Type: ')[1].split('Location: ')
+                incidentType = GoogleTranslator(source='en', target='ru').translate(united_list[0].strip())
+                incidentLocation_en = united_list[1].strip()
+                incidentLocation_ru = GoogleTranslator(source='en', target='ru').translate(incidentLocation_en)
+                incidentLocation_en = '(' + incidentLocation_en + ')'
 
-        elif 'Incident Type' in source_text[i]:
-            united_list = source_text[i].split('Incident Type: ')[1].split('Location: ')
-            incidentType = GoogleTranslator(source='en', target='ru').translate(united_list[0].strip())
-            incidentLocation_en = united_list[1].strip()
-            incidentLocation_ru = GoogleTranslator(source='en', target='ru').translate(incidentLocation_en)
-            incidentLocation_en = '(' + incidentLocation_en + ')'
-
-        elif source_text[i].strip()[-2].isalpha() and source_text[i].strip()[-1].isdigit():
-            if idc_flag:
-                incidentType += ';'
-            else:
-                idc_flag = True
-
-            incidentType += f'\nКатегория опасности\n(RS-G-1.9)-{source_text[i][-2]}'
-
-        elif 'ADDITIONAL' in source_text[i] and 'COMMENTS' in source_text[i]:
-            for n in range(1, 100):
-                if 'MATERIAL' not in source_text[i+n]:
-                    incidentDetectSummary += GoogleTranslator(source='en', target='ru').translate(source_text[i+n])
+            elif source_text[i].strip()[-2].isalpha() and source_text[i].strip()[-1].isdigit():
+                if idc_flag:
+                    incidentDangerCategories += ';'
                 else:
-                    break
+                    idc_flag = True
 
-    except IndexError:
-        pass
+                incidentDangerCategories += f'\nКатегория опасности\n(RS-G-1.9)-{source_text[i][-1]}'
 
+            elif 'ADDITIONAL' in source_text[i] and 'COMMENTS' in source_text[i]:
+                for n in range(1, 100):
+                    if 'MATERIAL' not in source_text[i+n]:
+                        incidentDetectSummary += GoogleTranslator(source='en', target='ru').translate(source_text[i+n])
+                    else:
+                        break
 
-print(itdbKey + '\n')
-print(webInfKey + '\n')
-print(incidentDate + '\n')
-print(incidentCountry_ru)
-print(incidentCountry_en + '\n')
-print(incidentLocation_ru)
-print(incidentLocation_en + '\n')
-print(incidentDetectSummary + '\n')
-print(incidentType + '\n')
+        except IndexError:
+            pass
+
+    return [itdbKey, webInfKey, incidentDate, incidentCountry_ru, incidentCountry_en, incidentLocation_ru,
+            incidentLocation_en, incidentDetectSummary, incidentType, incidentDangerCategories.strip()]
 
 # ADDITIONAL COMMENTS ABOUT THE MATERIAL
 # ADDITIONAL COMMENTS ABOUT THE MATERIAL
+docx_list = get_docx_info('2024-01-002.pdf')
+for elem in docx_list:
+    print(elem, end='\n\n')
